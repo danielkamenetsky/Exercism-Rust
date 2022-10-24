ddb1495e0b6ba61bce01a5d7ba7687d3be0ce13a
Daniel Kamenetsky <20dk19@queensu.ca>
2022-10-24 16:48:49 -0400

    Completed magazine-cutout exercism exercise

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,5 +1,5 @@
 // This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
+// To enable stricter warnings.
 #![allow(unused)]
 
 use std::collections::HashMap;
